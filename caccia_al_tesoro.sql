4a613792817f40e316541182592d2040afb6d2a8
LapoBiso <lapo.bisognin@stud.unifi.it>
2022-07-11 14:37:49 +0200

    linkato il css alla pagina

---
+++ caccia_al_tesoro.sql
@@ -0,0 +1,30 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+CREATE TABLE `players` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `nickname` text NOT NULL,
+  `game` int(6) NOT NULL,
+  `points` int(2) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+
+CREATE TABLE `paintings` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `title` text NOT NULL,
+  `author` text NOT NULL,
+  `year` text NOT NULL,
+  `technique` text NOT NULL,
+  `position` text NOT NULL,
+  `description` text NOT NULL,
+   PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+
+CREATE TABLE `matches` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `paintings` text NOT NULL,
+   PRIMARY KEY (`id`),
+     INDEX paintings_key (paintings),
+     FOREIGN KEY (paintings) REFERENCES paintings(id)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+
