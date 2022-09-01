71a6983dee0805e32415ca028efc810ef3d6cc92
LapoBiso <lapo.bisognin@stud.unifi.it>
2022-09-01 20:27:12 +0200

    Aggiunta tabella password e colonna url in paintings

--- server/paintings_actions_page.php
+++ server/paintings_actions_page.php
@@ -23,19 +23,20 @@
 	}
 
 	function insertPainting() {
-    	if (isset($_POST['title']) && isset($_POST['author']) && isset($_POST['year']) && isset($_POST['technique']) && isset($_POST['position'])&& isset($_POST['description'])) {
+    	if (isset($_POST['title']) && isset($_POST['author']) && isset($_POST['year']) && isset($_POST['technique']) && isset($_POST['position'])&& isset($_POST['description']) && isset($_POST['url'])) {
     		$title = $_POST['title'];
     		$author = $_POST['author'];
     		$year = $_POST['year'];
     		$technique= $_POST['technique'];
     		$position = $_POST['position'];
     		$description = $_POST['description'];
+    		$url = $_POST['url'];
     	} else {
     		echo "you didn't specify an attribute";
     		return;
     	}
     	$mysqli = new mysqli(DB_HOST,DB_USER, DB_PASSWORD,DB_DATABASE);
-    	$query_string = 'INSERT INTO  paintings(title, author, year, technique, position, description) VALUES ("' .$title .'", "' .$author .'", "' .$year .'", "' .$technique .'","' .$position .'","' .$description.'",)';
+    	$query_string = 'INSERT INTO  paintings(title, author, year, technique, position, description, url) VALUES ("' .$title .'", "' .$author .'", "' .$year .'", "' .$technique .'","' .$position .'","' .$description.'", "' .$url.'")';
     	$result=$mysqli->query($query_string);
        	$query_string = 'SELECT * FROM paintings WHERE id="' . $mysqli->insert_id .'"';
    		$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
