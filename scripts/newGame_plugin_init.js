c0ef0c9eb7cffa4811eb7312b6a7ea3d75fb1bb0
LapoBiso <lapo.bisognin@stud.unifi.it>
2022-09-07 19:50:30 +0200

    Aggiunta click wrap

--- scripts/newGame_plugin_init.js
+++ scripts/newGame_plugin_init.js
@@ -1,5 +1,3 @@
-
-
 $(document).ready(function() {
     console.log("INIT PLUGIN");
     jQuery(".newGameButton").createGame({serverURL : "server/matches_action_page.php", serverURL2 :"server/actions_page.php"}); //TODO creare relativo file actions e sostituire l'attuale con quello
