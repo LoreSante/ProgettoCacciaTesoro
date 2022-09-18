5dcb7aa137879d5d33c91ab008b39a07690fa4a6
LapoBiso <lapo.bisognin@stud.unifi.it>
2022-09-18 17:17:41 +0200

    Gallery 0.1 POST REVISIONE #1
    fix plugin enterGameButton_plugin.js, ora accetta nickname vuoti

--- scripts/enterGameButton_plugin.js
+++ scripts/enterGameButton_plugin.js
@@ -11,12 +11,15 @@
         });
 
         function sendPlayer() {
+            let nickname= $inputName.value;
+            if(nickname=="")
+                nickname="Guest";
             console.log("sendPlayer");
             let request_type = "insert";
             let request = $.ajax({
                 url: options.serverURL,
                 type: "POST",
-                data: {"text": $inputName.value, "game":99999 , "action": request_type},
+                data: {"text": nickname, "game":99999 , "action": request_type},
                 dataType: "json",
             });
             request.done(function(data) {
